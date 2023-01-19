902c188fcd4db1c3bf5b348b255c867970ea53ad
Jhon Real Luis <114674214+Guffawd@users.noreply.github.com>
2023-01-19 23:01:18 +0800

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class TictacToe {
     static char[][] board = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
     static boolean xTurn = true;
     static Scanner scanner = new Scanner(System.in);
